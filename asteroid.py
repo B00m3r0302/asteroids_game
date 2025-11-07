a16acbc98c60431ae4556b3733805c0137b60016
B00m3r0302 <hudson.t.woomer@gmail.com>
2025-11-07 14:03:13 -0500

    working on asteroid.py

---
+++ asteroid.py
@@ -0,0 +1,9 @@
+from circleshape import CircleShape
+import pygame
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+    
+    def draw(self):
+        pygame.draw.circle
