c2d25eb45b68b28131a022260f318c4e3c0a12c7
B00m3r0302 <hudson.t.woomer@gmail.com>
2025-11-07 11:58:15 -0500

    initial configuration complete

---
+++ main.py
@@ -0,0 +1,22 @@
+# This allows us to use code from the open-source
+# pygame library throughout this file
+import pygame
+from constants import *
+from logger import log_state
+def main():
+    print("Starting Asteroids!\nScreen width: 1280\nScreen height: 720")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+        
+
+
+if __name__ == "__main__":
+    main()
