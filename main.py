2508e1a56d371dc7eff582babef2191d3b039f42
B00m3r0302 <hudson.t.woomer@gmail.com>
2025-11-07 12:17:47 -0500

    passed the fps limit test

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ def main():
     print("Starting Asteroids!\nScreen width: 1280\nScreen height: 720")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+
         
 
 
