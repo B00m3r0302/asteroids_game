c71bdda1cbb73f3d2bd5466e630446a651e78632
B00m3r0302 <hudson.t.woomer@gmail.com>
2025-11-09 09:28:42 -0500

    matched boot.dev code

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ def main():
 
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    
+    dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
-    player1 = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     
     while True:
         log_state()
@@ -23,14 +25,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
+
         screen.fill("black")
         
-        Player.containers[0].update(dt)
-        for item in Player.containers[1]:
-            item.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         
         pygame.display.flip()
-        clock.tick(60)
+        
         dt = clock.tick(60) / 1000
 
         
