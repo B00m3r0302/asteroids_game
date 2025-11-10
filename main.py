940425911d95464560088199d6b6e634ccb41496
B00m3r0302 <hudson.t.woomer@gmail.com>
2025-11-10 09:10:39 -0500

    use seer stone at CH4 L 1

--- main.py
+++ main.py
@@ -1,45 +1,48 @@
-# This allows us to use code from the open-source
-# pygame library throughout this file
 import pygame
-from constants import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-from player import *
+from player import Player
+
+
 def main():
-    print("Starting Asteroids!\nScreen width: 1280\nScreen height: 720")
-
     pygame.init()
-    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-    
-    dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
-    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
-    
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
         log_state()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
         updatable.update(dt)
 
         screen.fill("black")
-        
+
         for obj in drawable:
             obj.draw(screen)
-        
+
         pygame.display.flip()
-        
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
-
-        
-
-        
 
 
 if __name__ == "__main__":
